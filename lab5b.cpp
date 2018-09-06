b339eb585acd6fe0577fa61f9cc921ea58b95435
isha-dwivedi <42205291+isha-dwivedi@users.noreply.github.com>
2018-09-06 09:29:56 -0400

    Add files via upload

---
+++ lab5b.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int i,j,k,a=5;
+for(i=0; i<a;i++)
+{
+for(j=0; j<a-i;j++)
+{cout<<" ";}
+for(j=0;j<a; j++)
+{cout<<"*";
+}
+
+
+
+
+cout<<endl;
+}
+}
