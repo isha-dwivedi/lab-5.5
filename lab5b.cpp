e47ca33fbf0ce2ac4d6f65788f8e4738cc00186a
isha-dwivedi <42205291+isha-dwivedi@users.noreply.github.com>
2018-09-06 09:33:50 -0400

    Add files via upload

---
+++ lab5b.cpp
@@ -0,0 +1,18 @@
+//rhombus star pattern
+#include<iostream>
+using namespace std;
+int main()
+{
+// declare the variables
+int i,j,a=5;
+//print the star pattern
+for(i=0; i<a;i++)
+{
+for(j=0; j<a-i;j++)
+{cout<<" ";}
+for(j=0;j<a; j++)
+{cout<<"*";
+}
+cout<<endl;
+}
+}
